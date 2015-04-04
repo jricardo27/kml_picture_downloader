ed6a5e79d26261132099f1b4d25f343e6ebf1b34
Ricardo Perez <jricardo27@gmail.com>
2015-04-04 12:26:48 +1100

    Download images

--- process_kml.py
+++ process_kml.py
@@ -8,6 +8,7 @@ use on Maverick by:
 from __future__ import absolute_import, print_function
 
 import os
+import urllib
 from pykml import parser
 
 
@@ -28,17 +29,54 @@ def get_parsed_file(filename):
     return parser.parse(open(filename, 'r')).getroot()
 
 
+def check_for_dir(path):
+    """
+    Check  if the given path exists, otherwise, create it
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def download_pic(url, img_name, dest_dir='photos'):
+    """
+    Download an image from the given url and store it on the specified dir
+    """
+    if ' ' in url:
+        url = url.split(' ')[0]
+
+    if '?' in url:
+        # Lets assume it's 'jpg'
+        extension = 'jpg'
+    else:
+        extension = url.split('.')[-1]
+
+    filename = '{0}.{1}'.format(img_name.encode('utf8'), extension)
+    filename = os.path.join(dest_dir, filename)
+
+    # If the file already exists, don't download it again
+    if not os.path.isfile((filename)):
+        print('Downloading: {0}'.format(url))
+        urllib.urlretrieve(url, filename)
+
+
 def main():
     """
     Main application
     """
+    check_for_dir('photos')
+
     for filename in get_kml_files():
         doc = get_parsed_file(filename)
 
+        print('Downloading pictures for {0}'.format(filename))
+
         for placemark in doc.Document.Folder.Placemark:
             try:
-                print(placemark.name.text)
-                print(placemark.ExtendedData.Data.value.text)
+                name = placemark.name.text
+                url = placemark.ExtendedData.Data.value.text
+
+                print('{0}'.format(name.encode('utf8')))
+                download_pic(url, name)
             except AttributeError:
                 pass
 
